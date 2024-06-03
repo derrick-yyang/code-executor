70dee8ac46caa3e3d6c10e19c81b7ae15a68aa30
Derrick Yang <yangguangzheng122@gmail.com>
2024-06-02 23:20:39 -0400

    pretty

--- code-execution-frontend/src/CodeEditor.tsx
+++ code-execution-frontend/src/CodeEditor.tsx
@@ -6,9 +6,7 @@ interface CodeEditorProps {
   setCode: (code: string) => void;
 }
 
-
 const CodeEditor: React.FC<CodeEditorProps> = ({ code, setCode }) => {
-
   return (
     <div className="w-1/2 bg-dark-gray text-white p-4 rounded-md overflow-y-auto">
       <Editor
@@ -16,7 +14,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ code, setCode }) => {
         defaultLanguage="python"
         defaultValue={code}
         onChange={(value) => setCode(value || "")}
-        theme='vs-dark'
+        theme="vs-dark"
       />
     </div>
   );
